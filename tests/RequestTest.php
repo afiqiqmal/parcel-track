e77e734b59ecd0e89aad193e4ece9bd2b7b23fc7
Hafiq <hafiqiqmal93@gmail.com>
2018-05-02 15:02:39 +0800

    test

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -3,9 +3,7 @@ namespace Tests;
 
 require_once __DIR__ .'/../vendor/autoload.php';
 
-use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase;
-use Xmhafiz\FbFeed\Request;
 /**
 * RequestTest.php
 * to test function in Request class
