c0a6363b0b427f78a9dd7a6f4d36bfebeb7f4c0b
Hafiq <hafiqiqmal93@gmail.com>
2018-05-04 20:16:13 +0800

    citylink completed

--- src/Tracker/CityLink.php
+++ src/Tracker/CityLink.php
@@ -29,38 +29,73 @@ class CityLink extends BaseTracker
 
     public function startCrawl($result)
     {
-//        $crawler = new Crawler($result['body']);
-//        $crawlerResult = $crawler->filter('#tr4'.$this->getTrackingNumber())
-//            ->filter('table tr')->each(function (Crawler $node, $i) {
-//            $result = $node->filter('td')->each(function (Crawler $node, $i) {
-//                return trim_spaces($node->text());
-//            });
-//            $data = [];
-//            foreach ($result as $key => $item) {
-//                if ($key == 0) {
-//                    $data['date'] = $item;
-//                }
-//                if ($key == 1) {
-//                    $data['process'] = $item;
-//                    $data['type'] = $this->distinguishProcess($item);
-//                }
-//
-//                if ($key == 2) {
-//                    $parcel = Carbon::createFromFormat("d M Y h:i a", $data['date']." ".$item);
-//                    $data['date'] = $parcel->toDateTimeString();
-//                    $data['timestamp'] = $parcel->timestamp;
-//                }
-//
-//                if ($key == 3) {
-//                    $data['event'] = $item;
-//                }
-//            }
-//
-//            return $data;
-//        });
-        echo $result['body'];
-        die();
+        $crawler = new Crawler($result['body']);
+//        $crawlerResult = $crawler->filter('#btmPanel')->html();
+        $crawlerResult = $crawler->filter('#btmPanel tr')->each(function (Crawler $node, $i) {
+            if (strpos($node->html(), 'tabletitle') !== false) {
+                $result = $node->filter('td')->each(function (Crawler $node, $i) {
+                    $value = trim_spaces($node->text());
+                    if (strlen($value) > 2) {
+                        return $value;
+                    }
+                });
 
-//        return $this->buildResponse($result, $crawlerResult);
+                return $this->removeAllNullInArray($result)[0];
+            }
+
+            if (strpos($node->html(), 'table_detail') !== false) {
+                $result = $node->filter('table tr')->each(function (Crawler $node, $i) {
+                    $result = $node->filter('td')->each(function (Crawler $node, $i) {
+                        $value = trim_spaces($node->text());
+                        if (strlen($value) > 2) {
+                            return $value;
+                        }
+                    });
+
+                    return $this->removeAllNullInArray($result);
+                });
+                return $this->removeAllNullInArray($result);
+            }
+        });
+
+        $crawlerResult = $this->removeAllNullInArray($crawlerResult);
+        $currentDate = null;
+        $finalResult = [];
+        foreach ($crawlerResult as $key => $item) {
+            //header
+            if ($key % 2 == 0) {
+                $currentDate = $item;
+            }
+
+            //detail
+            if ($key % 2 == 1) {
+                if (is_array($item)) {
+                    foreach ($item as $checkpoint) {
+                        $data = [];
+                        $parcel = Carbon::createFromFormat('l, F d, Y H:i a', $currentDate." ".$checkpoint[1]);
+                        $data['date'] = $parcel->toDateTimeString();
+                        $data['timestamp'] = $parcel->timestamp;
+                        $data['process'] = isset($checkpoint[0]) ? $checkpoint[0] : null;
+                        $data['type'] = $this->distinguishProcess($data['process']);
+                        $data['event'] = isset($checkpoint[2]) ? $checkpoint[2] : null;
+                        $finalResult[] = $data;
+                    }
+                }
+            }
+        }
+
+        return $this->buildResponse($result, $finalResult);
+    }
+
+    private function removeAllNullInArray($array)
+    {
+        $newData = [];
+        foreach ($array as $item) {
+            if ($item != null) {
+                $newData[] = $item;
+            }
+        }
+
+        return $newData;
     }
 }
