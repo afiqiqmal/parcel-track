7fbeaa74a01b39e6525abe4803564b23c9b00803
Hafiq <hafiqiqmal93@gmail.com>
2018-06-03 05:20:43 +0800

    UA

--- helper/helpers.php
+++ helper/helpers.php
@@ -8,7 +8,7 @@ define('PARCEL_METHOD_POST', 'POST');
 define('PARCEL_METHOD_GET', 'GET');
 define('PARCEL_METHOD_PATCH', 'PATCH');
 define('PARCEL_METHOD_DELETE', 'DELETE');
-define('PARCEL_USER_AGENT', 'parcel-tracker/1.0');
+define('PARCEL_USER_AGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
 
 if (! function_exists('parcel_track')) {
 
